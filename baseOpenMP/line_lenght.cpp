0d0d5174a77bb42724b13183888cb08caca1ef5e
Kamil <kamil.piotrowski@student.put.poznan.pl>
2016-12-14 23:06:10 +0100

    najnowsze zmiany

--- baseOpenMP/line_lenght.cpp
+++ baseOpenMP/line_lenght.cpp
@@ -12,118 +12,122 @@ int main(int argc, char* argv[])
 	double step;
 
 	time_t start, stop;
-	double x, pi, sum=0.0;
+	double x, pi, sum = 0.0;
 	int i;
-	step = 1./(double)num_steps;
+	step = 1. / (double)num_steps;
 
-   const int threads = 2;
-   const int calc_count = 20;
-   double times[calc_count];
-   volatile double sumTable[calc_count+2];
-   omp_set_num_threads(2);
+	const int threads = 2;
+	const int calc_count = 20;
+	double times[calc_count];
+	volatile double sumTable[calc_count + 2];
+	omp_set_num_threads(2);
 
-   //WERSJA 1:
-   /*
-   for (i = 0; i < calc_count; i++) {
-	   sumTable[i] = 0;
-	   sumTable[i + 1] = 0;
-	   sum = 0;
-	   start = clock();
-
-	   #pragma omp parallel private(x) shared(sumTable)
-	   {
-		   int th_num = omp_get_thread_num();
-		  #pragma omp for schedule(static,1)
-		  for (int j=0; j<num_steps; j++)
-		  {
-			 x = (j + .5)*step;
-			 sumTable[i+th_num] += 4.0/(1.+ x*x);
-		  }
-	   }
-
-	   sum = sumTable[i]+sumTable[i+1];
-
-	   pi = sum*step;
-	   stop = clock();
-	   times[i] = ((double)stop-start)/CLOCKS_PER_SEC;
-	}
-   for (i = 0; i<calc_count; i++) std::cout << times[i] << std::endl;
-	*/
-
-
-	//WERSJA 2
-
+	//WERSJA 1:
+	
 	for (i = 0; i < calc_count; i++) {
 		sumTable[i] = 0;
 		sumTable[i + 1] = 0;
-		  #pragma omp parallel private(start,stop) shared(sumTable)
-		  {
+		sum = 0;
+		start = clock();
+
+		#pragma omp parallel private(x) shared(sumTable)
+		{
+			int th_num = omp_get_thread_num();
+			HANDLE thread_handler = GetCurrentThread();
+			DWORD_PTR mask = (1 << (th_num % 2));
+
+			DWORD_PTR result = SetThreadAffinityMask(thread_handler, mask);
+		   #pragma omp for schedule(static,1)
+		   for (int j=0; j<num_steps; j++)
+		   {
+			  x = (j + .5)*step;
+			  sumTable[i+th_num] += 4.0/(1.+ x*x);
+		   }
+		}
+
+		sum = sumTable[i]+sumTable[i+1];
+
+		pi = sum*step;
+		stop = clock();
+		times[i] = ((double)stop-start)/CLOCKS_PER_SEC;
+	 }
+	for (i = 0; i<calc_count; i++) std::cout << times[i] << std::endl;
+	 
+
+
+	 //WERSJA 2
+	/*
+	for (i = 0; i < calc_count; i++) {
+		sumTable[i] = 0;
+		sumTable[i + 1] = 0;
+#pragma omp parallel private(start,stop) shared(sumTable)
+		{
 			start = clock();
-			  int th_num = omp_get_thread_num();
+			int th_num = omp_get_thread_num();
 
-			  for (int j = 0; j<num_steps; j++)
-			  {
-				  x = (j + .5)*step;
-				  sumTable[i + th_num] += 4.0 / (1. + x*x);
-			  }
+			for (int j = 0; j < num_steps; j++)
+			{
+				x = (j + .5)*step;
+				sumTable[i + th_num] += 4.0 / (1. + x*x);
+			}
 
-			  pi = sumTable[i + th_num]*step;
-			  stop = clock();
-			  times[i + th_num] = ((double)stop - start) / CLOCKS_PER_SEC;
-		  }
-		  printf("time1: %f,time2: %f\n", times[i], times[i+1]);
-	   }
+			pi = sumTable[i + th_num] * step;
+			stop = clock();
+			times[i + th_num] = ((double)stop - start) / CLOCKS_PER_SEC;
+		}
+		printf("time1: %f,time2: %f\n", times[i], times[i + 1]);
+	}*/
 
 
-	printf("Wartosc liczby PI wynosi %15.12f\n",pi);
+	printf("Wartosc liczby PI wynosi %15.12f\n", pi);
 	getchar();
 	return 0;
+
+
+	/**
+	WERSJA 2:
+	time1: 3.756000,time2: 3.787000
+	time1: 3.554000,time2: 3.557000
+	time1: 3.445000,time2: 3.478000
+	time1: 3.384000,time2: 3.426000
+	time1: 3.524000,time2: 3.517000
+	time1: 1.813000,time2: 1.840000 XXX
+	time1: 3.521000,time2: 3.539000
+	time1: 3.538000,time2: 3.569000
+	time1: 3.562000,time2: 3.602000
+	time1: 3.430000,time2: 3.439000
+	time1: 3.777000,time2: 3.772000
+	time1: 3.776000,time2: 3.768000
+	time1: 3.229000,time2: 3.240000
+	time1: 1.827000,time2: 1.749000 XXX
+	time1: 3.687000,time2: 3.658000
+	time1: 3.306000,time2: 3.272000
+	time1: 3.801000,time2: 3.803000
+	time1: 3.648000,time2: 3.662000
+	time1: 3.700000,time2: 3.704000
+	time1: 3.566000,time2: 3.568000
+
+	WERSJA 1:
+		1.063
+		0.759 XXX
+		1.048
+		1.046
+		1.057
+		1.046
+		1.057
+		1.05
+		1.045
+		0.756 XXX
+		1.042
+		1.05
+		1.051
+		1.043
+		1.053
+		1.05
+		1.034
+		0.756 XXX
+		1.05
+		1.046
+
+		Długość lini: 8słów, double: 8B => 64B*/
 }
-
-/**
-WERSJA 2:
-time1: 2.795000,time2: 2.637000
-time1: 3.695000,time2: 3.713000
-time1: 3.730000,time2: 3.730000
-time1: 3.693000,time2: 3.700000
-time1: 3.190000,time2: 3.230000
-time1: 1.832000,time2: 1.904000 XXX
-time1: 3.364000,time2: 3.390000
-time1: 3.493000,time2: 3.508000
-time1: 3.348000,time2: 3.312000
-time1: 3.314000,time2: 3.328000
-time1: 3.525000,time2: 3.530000
-time1: 3.571000,time2: 3.573000
-time1: 3.205000,time2: 3.226000
-time1: 1.972000,time2: 2.009000 XXX
-time1: 3.563000,time2: 3.579000
-time1: 3.423000,time2: 3.430000
-time1: 3.522000,time2: 3.516000
-time1: 3.448000,time2: 3.473000
-time1: 3.197000,time2: 3.200000
-time1: 2.916000,time2: 2.951000
-
-
-   WINDOWS:
-	1.084
-	0.549 XXX
-	0.777
-	0.749
-	0.738
-	0.759
-	0.689
-	0.653
-	0.699
-	0.48  XXX
-	0.854
-	0.799
-	0.796
-	0.775
-	0.804
-	0.721
-	0.812
-	0.562 XXX
-	0.702
-	0.719
-
-	Długość lini: 8słów, double: 8B => 64B
